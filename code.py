a4d437afe6219b24639057a2d67b062070e73a3f
hekai <hkstorm@163.com>
2017-11-07 22:42:21 +0800

    most information

--- code.py
+++ code.py
@@ -1,9 +1,1 @@
-coding 1st day
-coding 2end day 
-coding 4rd day 
-	dkskksladlasd
-	hello lpl
-	hellp hekai
-coding 5th day  moning
-	hello lxh
-	hello world
+git remote add origin git@github.com:hekaiqq/test.git
